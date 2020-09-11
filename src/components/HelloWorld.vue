8a1e7e8de92acaa14297eda74d51a290cd61d2df
wawlik <craazzy777@gmail.com>
2020-09-11 18:56:33 +0300

    component props changed

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,23 +1,12 @@
 <template>
 	<div class="hello">
 		<h1>{{ msg }}</h1>
-		<div v-for="(one, ind) in persons" :key="'person-' + one.id"
-			:class="'person ' + (colored === ind ? 'person--colored' :'')">
-			<div class="person-avatar--wrapper">
-				<img src="@/assets/hugh.png" alt="" class="person-avatar">
-			</div>
-			<div class="person-info">
-				<span class="person-info-name">{{one.name.toUpperCase()}} IS</span>
-				<div class="person-info-input-group">
-					<input-group 
-						:data="one"
-						@focus-gained="colored = ind"
-						@focus-lost="colored = null"
-						></input-group>
-					<span class="person-info-input-prefix">{{one.prefix}}</span>
-				</div>
-			</div>
-		</div>
+			<input-group 
+				v-if="inputData"
+				:data="inputData"
+				@focus-gained="colored = ind"
+				@focus-lost="colored = null"
+				></input-group>
 	</div>
 </template>
 
@@ -35,65 +24,17 @@ export default {
 	},
 	data(){
 		return{
-			persons: [],
-			colored: null
+			inputData: null,
 		}
 	},
 	mounted(){
-		this.persons = data;
+		this.inputData = data;
 	}
 }
 </script>
 
 <style>
-	.person{
-		display: flex;
-		margin-bottom: 20px;
-		justify-content: flex-start;
-		align-items: center;
-		color: #2c2c30;
-		font-family: 'Roboto', sans-serif;
-	}
 	.hello{
 		margin: auto;
-	}
-	.person-avatar--wrapper{
-		display: inline-block;
-		border-radius: 50%;
-		width: 104px;
-		height: 104px;
-	}
-	.person-avatar{
-		border-radius: 50%;
-		width: 100px;
-		height: 100px;
-		padding-left: 4px;
-	}
-	.person-info{
-		margin-left: 20px;
-		width: 100%;
-		text-align: left;
-	}
-	.person-info-input-group{
-		display: flex;
-		margin-top: 20px;
-	}
-	.person-info-input-prefix{
-		margin-left: 10px;
-	}
-	.person-info-name{
-        font-size: 13px;
-		font-weight: bold;
-		transition: all 0.25s;
-	}
-	.person--colored > .person-info-name--colored{
-		color: #2540ff;
-	}
-	.person--colored > .person-avatar--wrapper{
-		border: 2px solid #2540ff;
-		margin: -2px;
-	}
-	.person--colored > .person-info > .person-info-name{
-		color: #2540ff;
 	}
 </style>
--- src/components/inputGroup.vue
+++ src/components/inputGroup.vue
@@ -1,12 +1,23 @@
 <template>
 	<div class="inputGroup">
-		<input type="text" :id="'input-' + data.id"
-			v-model="count"
-			@click="$emit('focus-gained')"
-			@blur="$emit('focus-lost')"
-			style="width:50px;min-width:50px!important"
-			placeholder="7"
-			@input="getFormattedText($event.target.value, $event.target, $event)">
+		<div :class="'person ' + (focused ? 'person--colored' :'')">
+			<div class="person-avatar--wrapper">
+				<img src="@/assets/hugh.png" alt="" class="person-avatar">
+			</div>
+			<div class="person-info">
+				<span class="person-info-name">{{data.name.toUpperCase()}} IS</span>
+				<div class="person-info-input-group">
+					<input class="person-count--input" type="text" :id="'input-' + data.id"
+						v-model="count"
+						@click="focused = true"
+						@blur="focused = false"
+						style="width:50px;min-width:50px!important"
+						placeholder="7"
+						@input="getFormattedText($event.target.value, $event.target, $event)">
+					<span class="person-info-input-prefix">{{data.prefix}}</span>
+				</div>
+			</div>
+		</div>
 	</div>
 </template>
 
@@ -15,13 +26,17 @@
 export default {
 	name: 'inputGroup',
 	props: {
-		data: Object
+		data: {
+			type: Object,
+			required: true
+		}
 	},
 	data(){
 		return{
-			count: this.$props.data.count || '',
+			count: this.$props.data?.count || '',
 			// last value
-			vl: null
+			vl: null,
+			focused: null
 		}
 	},
 	methods: {
@@ -54,7 +69,8 @@ export default {
 				this.setCaretPos(target, tss+1);
 			}  else if (evt?.inputType === 'deleteContentBackward' || evt?.inputType === 'deleteContentForward'){
 				this.setCaretPos(target, tss);
-			} else if(evt?.inputType === 'insertText'){
+			} else if(evt?.inputType === 'insertText' && evt?.data){
+				console.log({evt})
 				// insert text save caret pos
 				if(tss !== vtls.length){
 					//get without spaces length
@@ -139,9 +155,56 @@ export default {
 }
 </script>
 
-<style scoped>
+<style >
 /* scoped to disable outer interactions of styles */
-	.inputGroup > input{
+	.person{
+		display: flex;
+		margin-bottom: 20px;
+		justify-content: flex-start;
+		align-items: center;
+		color: #2c2c30;
+		font-family: 'Roboto', sans-serif;
+	}
+	.person-avatar--wrapper{
+		display: inline-block;
+		border-radius: 50%;
+		width: 104px;
+		height: 104px;
+	}
+	.person-avatar{
+		border-radius: 50%;
+		width: 100px;
+		height: 100px;
+		padding-left: 4px;
+	}
+	.person-info{
+		margin-left: 20px;
+		width: 100%;
+		text-align: left;
+	}
+	.person-info-input-group{
+		display: flex;
+		margin-top: 20px;
+	}
+	.person-info-input-prefix{
+		margin-left: 10px;
+	}
+	.person-info-name{
+        font-size: 13px;
+		font-weight: bold;
+		transition: all 0.25s;
+	}
+	.person--colored > .person-info-name--colored{
+		color: #2540ff;
+	}
+	.person--colored > .person-avatar--wrapper{
+		border: 2px solid #2540ff;
+		margin: -2px;
+	}
+	.person--colored > .person-info > .person-info-name{
+		color: #2540ff;
+	}
+	.person-count--input{
 		border: none;
 		border-bottom: 1px solid #c9c9cf;
 		width: auto;
@@ -153,7 +216,7 @@ export default {
         color: #2c2c30;
 		font-family: 'Roboto', sans-serif;
 	}
-	.inputGroup > input:focus{
+	.person-count--input:focus{
 		border: none;
 		outline: none;
 		border-bottom: 1px solid #2540ff;
